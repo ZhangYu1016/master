997b8b8cc94a639ba1e787830b61b4edb9d5c033
ZhangYu1016 <1040601138@qq.com>
2019-04-26 00:20:58 +0800

    delete

--- baseSsm/src/main/java/com/factory/BaseBeanFactory.java
+++
@@ -1,8 +0,0 @@
-package com.factory;
-
-public interface BaseBeanFactory {
-
-	<T> T getBean(String name, Class<T> requiredType);
-	
-	boolean containsBean(String name);
-}
--- baseSsm/src/main/java/com/support/BaseConfigurableApplicationContext.java
+++
@@ -1,7 +0,0 @@
-package com.support;
-
-public interface BaseConfigurableApplicationContext extends BaseApplicationContext {
-
-	
-	
-}
