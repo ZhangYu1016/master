1c5efdccf598c7d0ef41dabebf07211e568a19db
1040601138@qq.com <1040601138@qq.com>
2019-05-12 16:09:36 +0800

    add

---
+++ ConfigClientApp/src/main/java/com/client/ConfigConfigApplication.java
@@ -0,0 +1,29 @@
+package com.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RefreshScope
+@EnableEurekaClient
+@RestController
+public class ConfigConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigConfigApplication.class, args);
+    }
+    @Value("${test}") // git配置文件里的key
+    String myww;
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return myww;
+    }
+
+}
