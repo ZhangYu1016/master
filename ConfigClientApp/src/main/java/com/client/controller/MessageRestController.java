4794b357f0ac508004e170d6266e7a47b33d1447
ZhangYu1016 <1040601138@qq.com>
2019-06-11 07:58:26 +0800

    add

--- ConfigClientApp/src/main/java/com/client/controller/MessageRestController.java
+++ ConfigClientApp/src/main/java/com/client/controller/MessageRestController.java
@@ -1,15 +1,10 @@
 package com.client.controller;
 
-import com.util.HttpRequest;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RefreshScope
 @RestController
@@ -30,11 +25,5 @@ public class MessageRestController {
         return test2;
     }
 
-    @RequestMapping(value = "/encrypt")
-    public String encrypt(String key){
-        System.out.println("需加密的明文:"+key);
-        String result = HttpRequest.sendPost("http://localhost:8961/encrypt",key);
-        System.out.println("获得密文:"+result);
-        return result;
-    }
+
 }
